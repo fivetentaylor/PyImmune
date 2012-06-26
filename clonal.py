acc2551cb7fc70750f9283e9e82e7eda30f804ef
Taylor Sather <fivetentaylor@gmail.com>
2012-06-25 22:10:44 -0700

    Basic functional interface
    We could definitely wrap all of these in a class, but I don't know
    if that's necessary for our limited scope.  Pretty easy to see what's
    going on from the function names.  I'm not sure this code is capturing
    the whole complexity of an AIS, but the basic GA structure is there.
    Definitely have a ways to go, but I think we're off to a good start.

--- clonal.py
+++ clonal.py
@@ -6,28 +6,50 @@ POP = 100
 GUESS = 5
 THRESHOLD = 16
 
+# functions
 def hamdist(str1, str2):
+    '''Stole from ActiveState, not the most efficient hamming distance function ever...'''
     diffs = 0
     for ch1, ch2 in zip(str1, str2):
         if ch1 != ch2:
             diffs += 1
     return diffs
 
+def initPop( size ):
+    i = 0
+    population = [0] * size
+    random.seed()
+    while i < size:
+        #print 'random: ' + str(random.getrandbits(64))
+        population[i] = random.getrandbits(32)
+        i += 1
+    return population
 
+def breed( pop ):
+    # spawn new children with some rule?
+    return
 
-# GLOBALS
-POP = 100
-GUESS = 5
+def fitness( antibody, antigen ):
+    # Something should be happening here with guessing on the antibodies part
+    # antibody and antigen should be integers
+    m = bin(antibody[0])[2:64].rjust(64,'0')
+    n = bin(anitgen[1])[2:64].rjust(64,'0')
+    return hamdist( m,n )
 
+def replace( pop, antigen ):
+    for i, member in pop:
+        val = fitness( member, antigen )
+        if val < THRESHOLD:
+            x[i] = 123 #new?  This is where mutation should happen
+    return pop
 
-
-i = 0
-x = [0] * POP
-random.seed()
-while i < POP:
-    #print 'random: ' + str(random.getrandbits(64))
-    x[i] = random.getrandbits(32)
-    i += 1
+def ga(pop, gen):
+    i = 0
+    antigen = 99 #???
+    while i < gen:
+        replace( pop, antigen )
+        i += 1
+        
 
 m = bin(x[0])[2:64].rjust(64,'0')
 n = bin(x[1])[2:64].rjust(64,'0')
