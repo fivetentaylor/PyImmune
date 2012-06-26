e33b0775230cad98a2f5dff80ca9620aff21e066
Taylor Sather <fivetentaylor@gmail.com>
2012-06-25 21:31:52 -0700

    Added a few comments

--- clonal.py
+++ clonal.py
@@ -1,5 +1,6 @@
 import random
 
+# Helper functions
 def hamdist(str1, str2):
     diffs = 0
     for ch1, ch2 in zip(str1, str2):
@@ -7,16 +8,18 @@ def hamdist(str1, str2):
             diffs += 1
     return diffs
 
-random.seed()
-popSize = 100
-i = 0
-x = [0] * popSize
 
 
 # GLOBALS
 POP = 100
 GUESS = 5
-while i < popSize:
+
+
+
+i = 0
+x = [0] * POP
+random.seed()
+while i < POP:
     #print 'random: ' + str(random.getrandbits(64))
     x[i] = random.getrandbits(32)
     i += 1
